6f7b2f54abfe7ce43d9e83127d104c5c24f262a7
mohamed <dhia.hsalem@gmail.com>
2024-05-17 14:18:13 +0200

    left and right paddle created

---
+++ main.py
@@ -0,0 +1,46 @@
+# to create the game the first i will have to:
+#create the pingpong_paddle
+#create the ball ,
+#make the ball move when it hits raket
+#create score list
+#create middle line
+#create screen
+
+
+from turtle import Turtle,Screen
+from paddle import Padle
+import time
+
+screen=Screen()
+screen.listen()
+screen.setup(height=600,width=800)
+screen.title("PingPong Game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+padle_right=Padle(350)
+padle_left=Padle(-350)
+
+
+screen.onkey(fun=padle_right.move_up,key="Up")
+screen.onkey(fun=padle_right.move_down,key="Down")
+screen.onkey(fun=padle_left.move_up,key="w")
+screen.onkey(fun=padle_left.move_down,key="a")
+game_is_on=True
+
+
+
+while game_is_on:
+    screen.update()
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,20 @@
+
+from turtle import Turtle
+
+class Padle(Turtle):
+    def __init__(self,position1):
+        super().__init__()
+        self.position1=position1
+        self.shape("square")
+        self.penup()
+        self.setpos(self.position1, 0)
+        self.shapesize(stretch_wid=5, stretch_len=0.5)
+        self.color("white")
+
+    def move_up(self):
+        if 240 > self.ycor():
+            self.sety(self.ycor() + 20)
+
+    def move_down(self):
+        if self.ycor() > -240:
+            self.sety(self.ycor() - 20)
